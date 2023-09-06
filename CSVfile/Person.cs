5dfb759fd6fee54146d1a4a91e7e03a177cbaf43
Gunel Qahramani <qahramanigunel@gamil.com>
2023-09-06 11:51:51 -0700

    first commit

---
+++ CSVfile/Person.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSVfile
+{
+    internal class Person
+    {
+
+        private string _firstname = "";
+        private string _lastname = "";
+        private string _occupation = "";
+        private int _age = 0;
+
+        public string FirstName { get { return _firstname; } }
+        public string LastName { get { return _lastname; } }
+        public string Ocupation { get { return _occupation; } }
+        public int Age { get { return _age; } }
+
+
+        public Person(string firstname, string lastname, string ocupation, int age)
+        {
+            _firstname = firstname;
+            _lastname = lastname;
+            _occupation = ocupation;
+            _age = age;
+        }
+    }
+}
